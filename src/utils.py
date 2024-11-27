fbeb3b55d7da35466b8519204b321b28a969bfde
PrasadMagdum08 <magdumprasad194@gmail.com>
2024-11-27 22:14:33 +0530

    utils

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,9 @@ from src.exception import CustomException
 
 import numpy as np
 import pandas as pd
-
 import dill
+
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path, obj):
@@ -16,6 +17,34 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_model(X_train, X_test, y_train, y_test, models):
+
+    """fitting and predict score the respected algorithms."""
+
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
